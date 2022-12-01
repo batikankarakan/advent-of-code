f506b1ed430c80d6cd618a58d33023991a2d5cda
Batikan <batikankarakan@gmail.com>
2022-12-01 19:25:40 +0300

    add first day of challenge

---
+++ 2022/day1/elf.php
@@ -0,0 +1,20 @@
+<?php
+
+$lines = file('inputfile.txt');
+$sum = 0;
+$elves = [];
+$result = 0;
+foreach ($lines as $line) {
+    if ($line == "\n") {
+        $elves[] = $sum;
+        $sum = 0;
+    }
+    $sum += intval($line);
+}
+echo "max value is: ".max($elves).PHP_EOL;
+rsort($elves);
+$sumArr = $elves[0] + $elves[1] + $elves[2];
+
+echo "max three value is: ".$sumArr.PHP_EOL;
+
+
